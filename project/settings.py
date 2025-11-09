f1e0d4af54a1ed69b75f72296b5245b614947ddc
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-11-08 23:33:52 -0300

    feat: installing features apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Features apps
+    "apps.goals",
+    "apps.bills",
 ]
 
 MIDDLEWARE = [
